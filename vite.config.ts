910322decb3f6875e6a040b3b0d487808f197d04
kayo barbosa <kayoo5916@gmail.com>
2026-04-18 10:05:48 -0300

    fix: clean build for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     build: {
-      outDir: "dist",
+      outDir: "dist/client",
     }
   }
 });
